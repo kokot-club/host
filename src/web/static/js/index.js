5f9e6f02d32cf265c8af8c4fca141e9785188dfc
catowice <catowice@users.noreply.github.com>
2026-01-11 19:02:25 +0100

    fix god awful tabbing

--- src/web/static/js/index.js
+++ src/web/static/js/index.js
@@ -2,39 +2,39 @@
 var languageCache = {}
 
 function getLanguage() {
-  const saved = localStorage.getItem('lang')
-  const lang = saved || 'en'
-  if (!saved) {
-    localStorage.setItem('lang', lang)
-  }
+    const saved = localStorage.getItem('lang')
+    const lang = saved || 'en'
+    if (!saved) {
+        localStorage.setItem('lang', lang)
+    }
 
-  return lang
+    return lang
 }
 
 function loadLanguage(lang) {
-  if (!languageCache[lang]) {
-    var req = new XMLHttpRequest()
-    req.open('GET', '/static/lang/' + lang + '.json', false)
-    req.send(null)
+    if (!languageCache[lang]) {
+      var req = new XMLHttpRequest()
+      req.open('GET', '/static/lang/' + lang + '.json', false)
+      req.send(null)
 
-    var txt = req.responseText || ''
+      var txt = req.responseText || ''
 
-    if (req.status === 200) {
-      try {
-        languageCache[lang] = JSON.parse(txt)
-      } catch (e) {
-        languageCache[lang] = {}
+      if (req.status === 200) {
+          try {
+              languageCache[lang] = JSON.parse(txt)
+          } catch (e) {
+              languageCache[lang] = {}
+          }
+      } else {
+          languageCache[lang] = {}
       }
-    } else {
-      languageCache[lang] = {}
     }
-  }
-  return languageCache[lang]
+    return languageCache[lang]
 }
 
 function t(str) {
-  var dict = loadLanguage(getLanguage())
-  return dict[str] || str
+    var dict = loadLanguage(getLanguage())
+    return dict[str] || str
 }
 
 // color schema manager
@@ -42,8 +42,8 @@ const root = document.documentElement
 let saved = localStorage.getItem('theme')
 
 if (!saved) {
-  saved = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-  localStorage.setItem('theme', saved)
+    saved = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    localStorage.setItem('theme', saved)
 }
 
 root.dataset.theme = saved

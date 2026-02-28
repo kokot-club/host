45438c4a124834f0d4f846fd254de94b6d277dc9
catowice <catowice@users.noreply.github.com>
2026-02-28 14:58:27 +0100

    fix users not being able to delete files

--- src/web/routes/files/__init__.py
+++ src/web/routes/files/__init__.py
@@ -106,7 +106,7 @@ def file_delete():
 
     file = File.from_uri(uri)
     if file:
-        if file.owner_id != user_id or user_level < UserRole.ADMIN.value:
+        if file.owner_id != user_id and user_level < UserRole.ADMIN.value:
             return jsonify({
                 'error': 'You are not allowed to delete this file'
             }), 403

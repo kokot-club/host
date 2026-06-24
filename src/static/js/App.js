f9c885914f5c4c614ef74c2db88c1c0e8463fcbb
catowice <catowice@users.noreply.github.com>
2026-06-24 10:58:46 +0200

    more auth clean up

--- src/static/js/App.js
+++ src/static/js/App.js
@@ -4,23 +4,22 @@ var ErrorPage = ErrorPage || {}
 var Home = Home || {}
 var Dashboard = Dashboard || {}
 
-const isLoggedIn = document.cookie.split('; ').find(row => row.startsWith('auth='))
 const hasRecoveryCode = new URLSearchParams(window.location.hash.split('?')[1]).get('recovery_code') != null
 
 m.route(document.body, '/', {
     '/': {
         view() {
-            return !isLoggedIn ? m.route.set('/login') : m(Dashboard)
+            return !IS_AUTHENTICATED ? m.route.set('/login') : m(Dashboard)
         }
     },
     '/login': {
         view() {
-            return isLoggedIn ? m.route.set('/') : m(Login)
+            return IS_AUTHENTICATED ? m.route.set('/') : m(Login)
         }
     },
     '/recovery': {
         view() {
-            return (isLoggedIn || hasRecoveryCode) ? m(Recovery) : m.route.set('/')
+            return (IS_AUTHENTICATED || hasRecoveryCode) ? m(Recovery) : m.route.set('/')
         }
     },
     '/error': {
